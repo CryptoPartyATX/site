27c608d8a9f96f6f907b8e6363632859aaaa1f45
plexiglass <plexiglass@riseup.net>
2014-06-13 17:52:49 -0500

    error checking

--- tweets.php
+++ tweets.php
@@ -79,77 +79,87 @@ link @'s and #'s
 // Why are we reading from a file? 
 // Because we don't want our visitors to have to connect to Twitter directly!
 $filename = "recent.txt";
-$initTweets = file($filename);
+$initTweets = @file($filename);
 
+if($initTweets !== FALSE){
 
-$outTweets = array();
-$numTweets = 16; // how many tweets to load, max about 16
+	$outTweets = array();
+	$numTweets = 16; // how many tweets to load, max about 16
 
-// from last 5 of input array, trim metadata and send to output array
-for($i=count($initTweets); $i>=count($initTweets)-$numTweets; $i--){
+	// from last 5 of input array, trim metadata and send to output array
+	for($i=count($initTweets); $i>=count($initTweets)-$numTweets; $i--){
 
-	// init temp var
-	$curTweet = $initTweets[$i];
+		// init temp var
+		$curTweet = $initTweets[$i];
 
-	// clean up the data
-	$curTweet = htmlspecialchars(trim(substr($curTweet,strpos($curTweet,">")+1)));
+		// clean up the data
+		$curTweet = htmlspecialchars(trim(substr($curTweet,strpos($curTweet,">")+1)));
 
-	// add to output array
-	if(strlen($curTweet)>0){array_push($outTweets,$curTweet);}
+		// add to output array
+		if(strlen($curTweet)>0){array_push($outTweets,$curTweet);}
 
-}
-// add follow div
-echo "<div class=\"follow\"><a href=\"https://twitter.com/atxcrypto\">@ATXCrypto</a> Recent Tweets</div>";
-
-// box in for scroll
-echo "<div class=\"tweetbox\">";
-
-// format and print tweets
-foreach ($outTweets as $tweet) {
-	echo "<div class=\"tweetitem\">";
-
-	// retweet icon
-	$RTicon = "<img class=\"rticon\" src=\"images/rticon.png\" />";
-	$isRT = substr($tweet,0,2)=="RT";
-	if($isRT){
-		//$tweet = substr($tweet,3);
-		echo $RTicon;
 	}
+	// add follow div
+	echo "<div class=\"follow\"><a href=\"https://twitter.com/atxcrypto\">@ATXCrypto</a> Recent Tweets</div>\n";
 
-	echo "<span class=\"atxcrypto\"><a href=\"https://twitter.com/atxcrypto\">@ATXCrypto</a>:</span> "; // account name
+	// box in for scroll
+	echo "<div class=\"tweetbox\">\n";
 
-	$linklist = "<br>";
+	// format and print tweets
+	foreach ($outTweets as $tweet) {
+		echo "<div class=\"tweetitem\">";
+
+		// retweet icon
+		$RTicon = "<img class=\"rticon\" src=\"images/rticon.png\" />";
+		$isRT = substr($tweet,0,2)=="RT";
+		if($isRT){
+			//$tweet = substr($tweet,3);
+			echo $RTicon;
+		}
+
+		echo "<span class=\"atxcrypto\"><a href=\"https://twitter.com/atxcrypto\">@ATXCrypto</a>:</span> "; // account name
+
+		$linklist = "<br>";
 	
-	// extract links
-	while(strpos($tweet,"http:")!==FALSE||strpos($tweet,"https:")!==FALSE){
-		$firstHTTP = strpos($tweet,"http");
-		$extracted = "";
-		$url="";
-		$isHTTPS = false;
-		if(strpos($tweet,"http:")===$firstHTTP || strpos($tweet,"https:")===$firstHTTP && $firstHTTP !== FALSE){
-			$isHTTPS = strpos($tweet,"https:")===$firstHTTP;
-			$url = substr($tweet,$firstHTTP,strpos("$tweet "," ",$firstHTTP));
-			$tweet = substr($tweet,0,$firstHTTP) . substr($tweet,strpos($tweet." "," ",$firstHTTP));
+		// extract links
+		while(strpos($tweet,"http:")!==FALSE||strpos($tweet,"https:")!==FALSE){
+			$firstHTTP = strpos($tweet,"http");
+			$extracted = "";
+			$url="";
+			$isHTTPS = false;
+			if(strpos($tweet,"http:")===$firstHTTP || strpos($tweet,"https:")===$firstHTTP && $firstHTTP !== FALSE){
+				$isHTTPS = strpos($tweet,"https:")===$firstHTTP;
+				$url = substr($tweet,$firstHTTP,strpos("$tweet "," ",$firstHTTP));
+				$tweet = substr($tweet,0,$firstHTTP) . substr($tweet,strpos($tweet." "," ",$firstHTTP));
 			
 			
 	
-			if($isHTTPS){
-				$linklist .= "<a class=\"tweetHTTPS\" href=\"$url\">[ HTTPS Link ]</a> \n";
-			}
-			else {
-				$linklist .= "<a class=\"tweetHTTP\" href=\"$url\">[ HTTP Link ]</a> \n";
+				if($isHTTPS){
+					$linklist .= "<a class=\"tweetHTTPS\" href=\"$url\">[ HTTPS Link ]</a> \n";
+				}
+				else {
+					$linklist .= "<a class=\"tweetHTTP\" href=\"$url\">[ HTTP Link ]</a> \n";
+				}
 			}
 		}
+	
+		if(trim($tweet) === ""){ $tweet = " [Link only] "; }
+
+		echo "<span class=\"tweet\">$tweet</span>\n"; // actual text (minus links)
+
+		// include links after tweet
+		echo $linklist;
+	
+		echo "</div>\n";
 	}
-	
-	if(trim($tweet) === ""){ $tweet = " [Link only] "; }
-
-	echo "<span class=\"tweet\">$tweet</span>\n"; // actual text (minus links)
-
-	// include links after tweet
-	echo $linklist;
-	
-	echo "</div>";
+	echo "</div>\n";
 }
-echo "</div>";
+else{
+	echo "<div class=\"follow\"><a href=\"https://twitter.com/atxcrypto\">@ATXCrypto</a> Recent Tweets</div>\n";
+	// box in for scroll
+	echo "<div class=\"tweetbox\">\n";
+	echo "<span class=\"tweet\">An error occured. Tweets were not loaded.</span>\n";
+	echo "</div>\n";
+	
+}
 ?>
